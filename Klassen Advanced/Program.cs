6a1d0a68a3c8c1849b5c18f15061f9ce347295fd
Diederik Van Gogh <doctor_music@hotmail.com>
2021-01-15 20:46:55 +0100

    Change Win Form to Console App

---
+++ Klassen Advanced/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Klassen_Advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
